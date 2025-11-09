568576f9e36bb438181218e36bedb4872f332b30
DanhTranCong <trancongdanh99@gmail.com>
2025-11-09 22:03:10 +0700

    Change Header

--- components/aqi-header/AqiHeader.jsx
+++ components/aqi-header/AqiHeader.jsx
@@ -7,7 +7,7 @@ export default function AqiHeader() {
       <h1 className="aqi-header-title">
         Môn học: Khai thác dữ liệu và ứng dụng
       </h1>
-      <h2 className="aqi-header-sub">Nhóm 13</h2>
+      <h2 className="aqi-header-sub">Nhóm 8</h2>
       <p className="aqi-header-topic">
         Chủ đề:{" "}
         <strong>Xây dựng mô hình dự đoán chất lượng không khí (AQI)</strong>
